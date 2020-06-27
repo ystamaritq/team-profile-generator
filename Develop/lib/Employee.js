09236c1509e557cc3aadba096c425dd4c19d75e3
Yadira Tamarit <ystamaritq@gmail.com>
2020-06-27 14:38:03 -0500

    added class employee with properties and methods

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,17 @@
 // TODO: Write code to define and export the Employee class
+
+class Employee {
+	constructor(name, id, email) {
+		this.name = name;
+		this.id = id;
+		this.email = email;
+
+		this.getName = function () {};
+
+		this.getId = function () {};
+
+		this.getEmail = function () {};
+
+		this.getRole = function () {}; // Returns 'Employee' */
+	}
+}
