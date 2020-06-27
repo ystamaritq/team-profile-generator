00515393ce131735cb49754be1f5c0957205e0da
Yadira Tamarit <ystamaritq@gmail.com>
2020-06-27 13:30:45 -0500

    added Utils folder with common js files using by the project

---
+++ Develop/lib/Utils.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const fs = require("fs");
+
+const handleError = (err) => {
+	if (err) console.log("There was an error: " + err);
+};
+
+const validateOutputDir = (outputDir) => {
+	// validate if output dir exists, otherwise createthis
+	fs.exists(outputDir, (exists) => {
+		if (!exists) fs.mkdir(outputDir, handleError);
+	});
+};
+
+const writeOutputFile = (outputPath, data) => {
+	fs.writeFile(outputPath, data, handleError);
+};
+
+module.exports = {
+	validateOutputDir: validateOutputDir,
+	writeOutputFile: writeOutputFile,
+};
